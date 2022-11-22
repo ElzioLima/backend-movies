7335bdd82a33377e940450b6cb941eb6022bcb7e
Elzio Nogueira Lima <elziolimach@gmail.com>
2022-11-21 20:03:34 -0400

    remove swagger console log

--- src/main/config/swagger.ts
+++ src/main/config/swagger.ts
@@ -5,6 +5,5 @@ import { Express } from "express"
 import { noCache } from "../middlewares"
 
 export default (app: Express): void => {
-  console.log("\n\nSWAGGER\n\n")
   app.use("/docs", noCache, serve, setup(swaggerConfig))
 }
